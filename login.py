d6aa589e7f72c2e24b483facf7ce606a7f957a9d
WangZhen <wangzhenjjcn@gmail.com>
2019-02-22 14:30:12 +0800

    v1

--- login.py
+++ login.py
@@ -1,11 +1,12 @@
 #coding=utf-8
 import sys,time,datetime,os,requests
-
+import json
 import pytesseract
 import base64
 # import rsa
-from Cryptodome.Cipher import PKCS1_v1_5
-from Cryptodome.PublicKey import RSA
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+import base64
 from PIL import Image
 from random import random
  
@@ -83,41 +84,194 @@ def getPublicKey(data):
         value=data[data.index("pubkey"):]
         value=value[value.index("\'")+1:]
         value=value[:value.index("\'")]
-        print(value)
         return value
     else:
         return ""
 
- 
+
+def handle_pub_key(key):
+    start = '-----BEGIN PUBLIC KEY-----\n'
+    end = '-----END PUBLIC KEY-----'
+    result = ''
+    # 分割key，每64位长度换一行
+    divide = int(len(key) / 64)
+    divide = divide if (divide > 0) else divide+1
+    line = divide if (len(key) % 64 == 0) else divide+1
+    for i in range(line):
+        result += key[i*64:(i+1)*64] + '\n'
+    result = start + result + end
+    return result
+
+
+def encrypt(content,key):
+    pub_key = handle_pub_key(key)
+    pub = RSA.import_key(pub_key)
+    cipher = PKCS1_v1_5.new(pub)
+    encrypt_bytes = cipher.encrypt(content.encode(encoding='utf-8'))
+    result = base64.b64encode(encrypt_bytes)
+    result = str(result, encoding='utf-8')
+    return result 
  
 def doLogin(username,password):
     passportPage=openPage(url)
     postPassportSSOShowTag(passportPage)
     boxStatus = "false"
-    print(boxStatus)
     isInput="true"
     token = ""
     glCaptchaToken=getValueById("glCaptchaToken",passportPage)
     baclUrl=getValueById("backUrl",passportPage)
     sysname = getValueById("sysname",passportPage)
-    print(sysname)
-    print(glCaptchaToken)
-    print(baclUrl)  
-    print(isInput)  
-    print(token)  
     publicKey=getPublicKey(passportPage)
-    print(publicKey)
-    key = RSA.import_key(publicKey)
-    passwd = PKCS1_v1_5.new(key)
-    # text = base64.b64encode(passwd.encrypt(bytes(password, encoding='utf-8')))
-    # password_rsa=rsa.encrypt(password, key)
-    print(str(passwd))
+    password_rsa=encrypt(password,publicKey)
+    timenow=(int(round(time.time() * 1000)))
+    loginUrl="http://passport.yaoex.com/passport/sso/pc_login"
+    postData = { 
+        "boxStatus": boxStatus,
+        "isInput": isInput,
+        "pc_userToken": token,
+        "username": username,
+        "password": password_rsa,
+        "glCaptchaToken": glCaptchaToken,
+        "sysname": sysname,
+        "time": timenow,
+        "backUrl":baclUrl
+        }
+    responseRes = webSession.post(loginUrl, data = postData, headers = defaultHeader,verify=False )
+    webSession.cookies.save()
+    print(f"statusCode = {responseRes.status_code}")
+    # print(f"text = {responseRes.text}")
+    return responseRes.text
+
+def getMainPage():
+    mainPageUrl="http://mall.yaoex.com/?source=1"
+    print("mainPageUrl:"+mainPageUrl)
+    responseRes = webSession.get(mainPageUrl,  headers = defaultHeader)
+    print(f"statusCode = {responseRes.status_code}")
+    # print(f"text = {responseRes.text}")
+    webSession.cookies.save()
+    return responseRes.text
+
+def getLoginIfo():
+    LoginInfoUrl="http://mall.yaoex.com/index/login"
+    print("LoginInfoUrl:"+LoginInfoUrl)
+    responseRes = webSession.get(LoginInfoUrl,  headers = defaultHeader)
+    print(f"statusCode = {responseRes.status_code}")
+    # print(f"text = {responseRes.text}")
+    webSession.cookies.save()
+    return responseRes.text
+
+def reLogin():
+    print("输入用户名：")
+    username=input()
+    print("输入密码：")
+    password=input()
+    login_msg=doLogin(username, password)
+    if("\"successful\":true"in login_msg):
+        print("登陆成功！")
+    else:
+        print("登录失败！！")
+        print(login_msg)
+
+
+def getCategoryList():
+    categoryListUrl="http://mall.yaoex.com/catg/categoryList"
+    print("categoryListUrl:"+categoryListUrl)
+    responseRes = webSession.get(categoryListUrl,  headers = defaultHeader)
+    print(f"statusCode = {responseRes.status_code}")
+    # print(f"text = {responseRes.text}")
+    webSession.cookies.save()
+    return responseRes.text
+
+def getCategoryListByCode(code):
+    categoryListUrl="http://mall.yaoex.com/catg/secondCategoryList?code="+code
+    print("categoryListUrl:"+categoryListUrl)
+    responseRes = webSession.get(categoryListUrl,  headers = defaultHeader)
+    print(f"statusCode = {responseRes.status_code}")
+    # print(f"text = {responseRes.text}")
+    webSession.cookies.save()
+    return responseRes.text
+
+def postSearchProductList(product2ndLMCode,nowPage):
+    searchProductListUrl="http://gateway-b2b.fangkuaiyi.com/api/search/searchProductList"
+    print("searchProductListUrl:"+searchProductListUrl)
+    postData = { 
+        "tradername": "yaoex_pc",
+        "trader": "pc",
+        "closesignature": "yes",
+        "signature_method": "md5",
+        "signature": "****",
+        "timestamp": (int(round(time.time() * 1000))),
+        "inputCharset": "utf-8",
+        "charsetColumns": "keyword",
+        "keyword": "",
+        "decodeKeyword":  "" ,
+        "userId":  "",
+        "roleId":  "",
+        "userType":  "",
+        "buyerCode": "" ,
+        "sellerCode": "" ,
+        "spuCode":  "",
+        "product2ndLMCode": product2ndLMCode,
+        "haveGoodsTag": "false",
+        "promotionTag": "false",
+        "buyerHistoryTag": "false",
+        "templateId":  "",
+        "factoryIds":  "",
+        "sellerCodes":  "",
+        "sortColumn": "default",
+        "sortMode": "desc",
+        "nowPage": nowPage,
+        "per": 10,
+        "invokeType":  "",
+        "drugName":  "",
+        "factoryName": "" ,
+        "spec": "" ,
+        "sellerName":  "",
+        }
+    responseRes = webSession.post(searchProductListUrl, data = postData, headers = defaultHeader,verify=False )
+    webSession.cookies.save()
+    print(f"statusCode = {responseRes.status_code}")
+    # print(f"text = {responseRes.text}")
+    return responseRes.text
 
 
 if __name__ == "__main__":
     webSession.cookies.load()
     print(webSession.cookies)
-    doLogin("test", "123456")
+    mainpage=getMainPage()
+    loGinInfo=getLoginIfo()
+    # if("var showName = \'\'" in loGinInfo ):
+    #     reLogin()
+    # loGinInfo=getLoginIfo()
+    categoryListData=getCategoryList()
+    categoryList=json.loads(categoryListData)
+    if ("success" in categoryList['status']):
+        print(len(categoryList['data']))
+        for category in categoryList['data']:
+            print("读取："+category['fixCategoryName0']+"次级目录中")
+            category2ListData=getCategoryListByCode(category['code'])
+            category2List=json.loads(category2ListData)
+            if ("success" in category2List['status']):
+                print(category2List['data']['snd_catagory'])
+                for category2 in category2List['data']['snd_catagory']:
+                    print("读取："+category2['name']+"次级目录中")
+                    category3ListData=getCategoryListByCode(category2['code'])
+                    category3List=json.loads(category3ListData)
+                    if ("success" in category3List['status']):
+                        print(len(category3List['data']['snd_catagory']))
+                        for category3 in category3List['data']['snd_catagory']:
+                            print("读取："+category3['name']+"次级目录中")
+    else:
+        print("Login err")
+
+     
+    
+    # if ("登录状态异常" in categoryList['rtn_msg']):
+    #     print("Login err")
+    # else:
+    #     print(len(categoryList['data']))
+
+ 
 
 
 
